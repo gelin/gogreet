7485f7062babe871f9dc7590743ab0349512c5a8
Denis Nelubin <dnelubin@gmail.com>
2019-08-03 16:47:00 +0600

    Added print to test

--- greet_test.go
+++ greet_test.go
@@ -1,9 +1,13 @@
 package greet
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGreetingFor(t *testing.T) {
 	result := GreetingFor("World")
+	fmt.Println(result)
 	if result != "Hello, World!" {
 		t.Fatal(result)
 	}
